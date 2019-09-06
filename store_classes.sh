9a8ce6054e045744e00850cd827ca1d05a9f51df
sam-lev <samlev@cs.utah.edu>
2019-09-05 21:15:10 -0600

    Code and exec to format image data as used in Cifar, dict with image as single vector of data:[num_images,r,g,b] label:[class label for im at same index]

---
+++ store_classes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#python storeData.py -dirdl ./data_og/DLBCL/SP-17-30422 -pout ./data -batch_name SP-17-30422-dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/SP-18-5335 -pout ./data -batch_name SP-18-5335-dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/SP-19-2811  -pout ./data -batch_name SP-19-2811-dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/DLBCL_Case_6  -pout ./data -batch_name case_6_dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/DLBCL_Case_2  -pout ./data -batch_name case_2_dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/DLBCL_Case_12  -pout ./data -batch_name case_12_dlbcl
+
+python storeData.py -dirdl ./data_og/DLBCL/DLBCL_Case_11  -pout ./data -batch_name case_11_dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/DLBCL_Case_19  -pout ./data -batch_name case_19_dlbcl
+
+#python storeData.py -dirdl ./data_og/DLBCL/SP-18-28908  -pout ./data -batch_name SP-18-28908_dlbcl
+
+#>>>>>>> BL
+
+#python storeData.py -dirbl ./data_og/BL/HP-18-706 -pout ./data -batch_name bl-HP-18-706
+
+#python storeData.py -dirbl ./data_og/BL/HP-11-860 -pout ./data -batch_name bl-HP-11-860
+
+#python storeData.py -dirbl ./data_og/BL/HP-19-161-PCS-19-545 -pout ./data -batch_name bl-HP-19-161-PCS-19-545
+
+#python storeData.py -dirbl ./data_og/BL/HP-17-113 -pout ./data -batch_name bl-HP-17-113
