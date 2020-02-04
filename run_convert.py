529236a7e1a22add995b8d74508c09a58414a08f
sam-lev <samlev@cs.utah.edu>
2020-02-03 17:51:26 -0700

    script to convert data into idx through lymphomadataflow

---
+++ run_convert.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from LyCNN.datapack.IO.Convert_ZIDX import Convert_Dataset_ZIDX
+from LyCNN.datapack.IO.VisusDataflow import ShowData
+read_path = "/home/sam/Documents/PhD/Research/Convolutional_Neural_Net_Lymphoma/data/train"
+write_path = "/home/sam/Documents/PhD/Research/Convolutional_Neural_Net_Lymphoma/data/train/train_idx"
+
+
+if ((len(sys.argv) > 1) and bool(int(sys.argv[1]))):
+    Convert_Dataset_ZIDX(read_path=read_path, write_path=write_path)
+
+cat_sample ="/home/sam/anaconda3/lib/python3.7/site-packages/OpenVisus/datasets/cat/visus.idx"
+sample = os.path.join(write_path,"batch_data_DLBCL_Case_10_2.idx")
+
+resolution = None if len(sys.argv) <= 2 else int(sys.argv[2])
+
+ShowData(data=sample,load=True, resolution=resolution, print_attr=1)
